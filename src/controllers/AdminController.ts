1ec5d6f1c569717174ff627012fa8812b4a68e43
zhaowei <314067902@qq.com>
2023-06-26 17:26:20 +0800

    docker环境报错

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -5,6 +5,7 @@ class AdminController {
     public add(req:Request,res:Response){
         const adminService = new AdminService()
         try {
+            console.log(req.body)
             const result = adminService.add(req.body)
             res.send(ResultJson.getSuccessData(result))
         } catch (error) {
